e69a1ef75d769ac5a7d210a1f58fc268a187be64
Roberval Walsh <roberval.walsh@desy.de>
2020-05-14 00:08:21 +0200

    class for dryair forms

---
+++ app/dryair_forms.py
@@ -0,0 +1,11 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, DecimalField, BooleanField, SubmitField, TimeField
+from wtforms.validators import DataRequired
+
+class DryAirForm(FlaskForm):
+    name = StringField('Name', validators=[DataRequired()])
+    temperature = DecimalField('Temperature', validators=[DataRequired()])
+    var1 = DecimalField('Var1', validators=[DataRequired()])
+    var2 = DecimalField('Var1', validators=[DataRequired()])
+    var3 = DecimalField('Var1', validators=[DataRequired()])
+    time_read = TimeField('Reading Time', validators=[DataRequired()])
