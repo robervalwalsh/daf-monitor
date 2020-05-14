c05cdc85d22b89bf4542a966e7359ee7699a9347
Roberval Walsh <roberval.walsh@desy.de>
2020-05-14 13:26:54 +0200

    additional fields, defaults

--- app/dryair/forms.py
+++ app/dryair/forms.py
@@ -1,16 +1,17 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, DecimalField, BooleanField, SubmitField, TimeField, DateTimeField, SelectField
 from wtforms.validators import DataRequired
+from wtforms.widgets import TextArea
+from datetime import datetime
 
 class DryAirForm(FlaskForm):
-    location = SelectField('Location', choices=[(0, "Building 25c"), (1, "Building 26")], validators=[DataRequired()])
+    location = SelectField('Location', default=0, choices=[(0, "Select one location"), (1, "Building 25c"), (2, "Building 26")], validators=[DataRequired()])
     author = StringField('Author', validators=[DataRequired()])
-    dewpoint = DecimalField('Dew point', validators=[DataRequired()])
-#    var1 = DecimalField('Var1', validators=[DataRequired()])
-#    var2 = DecimalField('Var1', validators=[DataRequired()])
-#    var3 = DecimalField('Var1', validators=[DataRequired()])
-    date = DateTimeField('Date and time', format='%d.%m.%Y %H:%M:%S', validators=[DataRequired()])
-    runtime = DecimalField('Runtime total (???)', validators=[DataRequired()])
-    runtime1 = DecimalField('Runtime unit 1', validators=[DataRequired()])
-    runtime2 = DecimalField('Runtime unit 2', validators=[DataRequired()])
-    submit = SubmitField('Submit')
+    date = DateTimeField('Date and time', default=datetime.today, validators=[DataRequired()])
+    dewpoint = DecimalField('Dew point (\xb0C)', validators=[DataRequired()])
+    runtime = DecimalField('Runtime total (hours)', validators=[DataRequired()])
+    runtime1 = DecimalField('Runtime unit 1 (hours)', validators=[DataRequired()])
+    runtime2 = DecimalField('Runtime unit 2 (hours)', validators=[DataRequired()])
+    message = StringField('Message (optional)', widget=TextArea())
+    submit = SubmitField('Add entry')
+    clear = SubmitField('Clear')
