5e2eb9e4274c3de5c833f763af3836880325c2f3
Roberval Walsh <roberval.walsh@desy.de>
2020-05-14 10:54:34 +0200

    runtime fields

--- app/dryair/forms.py
+++ app/dryair/forms.py
@@ -1,12 +1,16 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, DecimalField, BooleanField, SubmitField, TimeField
+from wtforms import StringField, DecimalField, BooleanField, SubmitField, TimeField, DateTimeField, SelectField
 from wtforms.validators import DataRequired
 
 class DryAirForm(FlaskForm):
-    name = StringField('Name', validators=[DataRequired()])
-    temperature = DecimalField('Temperature', validators=[DataRequired()])
-    var1 = DecimalField('Var1', validators=[DataRequired()])
-    var2 = DecimalField('Var1', validators=[DataRequired()])
-    var3 = DecimalField('Var1', validators=[DataRequired()])
-    time_read = TimeField('Reading Time', validators=[DataRequired()])
+    location = SelectField('Location', choices=[(0, "Building 25c"), (1, "Building 26")], validators=[DataRequired()])
+    author = StringField('Author', validators=[DataRequired()])
+    dewpoint = DecimalField('Dew point', validators=[DataRequired()])
+#    var1 = DecimalField('Var1', validators=[DataRequired()])
+#    var2 = DecimalField('Var1', validators=[DataRequired()])
+#    var3 = DecimalField('Var1', validators=[DataRequired()])
+    date = DateTimeField('Date and time', format='%d.%m.%Y %H:%M:%S', validators=[DataRequired()])
+    runtime = DecimalField('Runtime total (???)', validators=[DataRequired()])
+    runtime1 = DecimalField('Runtime unit 1', validators=[DataRequired()])
+    runtime2 = DecimalField('Runtime unit 2', validators=[DataRequired()])
     submit = SubmitField('Submit')
