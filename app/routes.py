cd67f936f804e02d088459585feeb5e25585506a
Roberval Walsh <roberval.walsh@desy.de>
2020-05-14 10:54:47 +0200

    updates

--- app/routes.py
+++ app/routes.py
@@ -1,12 +1,15 @@
 from flask import render_template
 from app import app
+from app.dryair.forms import DryAirForm
 
 @app.route('/daf-monitor/')
 @app.route('/daf-monitor/home')
 def home():
     return render_template('home.html',title='Home')
 
-@app.route('/daf-monitor/dryair')
+@app.route('/daf-monitor/dryair/add')
 def dryair():
     title = 'Dry Air'
-    return render_template('dryair.html',title=title)
+    form = DryAirForm()
+    locations = ['25c','26']
+    return render_template('dryair.html',title=title, locations=locations, form=form)
