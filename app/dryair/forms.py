509edff65ce5d6feee5d0c677624e5c1b63f3808
Roberval Walsh <roberval.walsh@desy.de>
2020-05-15 08:52:29 +0200

    saving data to cvs files

--- app/dryair/forms.py
+++ app/dryair/forms.py
@@ -1,17 +1,48 @@
+import os.path
+
 from flask_wtf import FlaskForm
 from wtforms import StringField, DecimalField, BooleanField, SubmitField, TimeField, DateTimeField, SelectField
 from wtforms.validators import DataRequired
 from wtforms.widgets import TextArea
 from datetime import datetime
+import pandas as pd
 
 class DryAirForm(FlaskForm):
-    location = SelectField('Location', default=0, choices=[(0, "Select one location"), (1, "Building 25c"), (2, "Building 26")], validators=[DataRequired()])
-    author = StringField('Author', validators=[DataRequired()])
-    date = DateTimeField('Date and time', default=datetime.today, validators=[DataRequired()])
-    dewpoint = DecimalField('Dew point (\xb0C)', validators=[DataRequired()])
-    runtime = DecimalField('Runtime total (hours)', validators=[DataRequired()])
-    runtime1 = DecimalField('Runtime unit 1 (hours)', validators=[DataRequired()])
-    runtime2 = DecimalField('Runtime unit 2 (hours)', validators=[DataRequired()])
-    message = StringField('Message (optional)', widget=TextArea())
-    submit = SubmitField('Add entry')
-    clear = SubmitField('Clear')
+    submit = SubmitField('  Submit Form  ')
+    # building 26
+    date_25c = DateTimeField('Date', default=datetime.today, validators=[DataRequired()])
+    dewpoint_25c = DecimalField('Dew point (\xb0C)', validators=[DataRequired()])
+    system_25c = DecimalField('System (hours)', validators=[DataRequired()])
+    pump1_25c = DecimalField('Pump 1 (hours)', validators=[DataRequired()])
+    pump2_25c = DecimalField('Pump 2 (hours)', validators=[DataRequired()])
+    # building 26
+    date_26 = DateTimeField('Date', default=datetime.today, validators=[DataRequired()])
+    dewpoint_26 = DecimalField('Dew point (\xb0C)', validators=[DataRequired()])
+    compressor_26 = DecimalField('Compressor (hours)', validators=[DataRequired()])
+    
+    def save_data(self):
+        # Building 25c
+        data25c = { 'date': [self.date_25c.data.strftime('%Y-%m-%d %H:%M:%S')],
+                    'system': [str(self.system_25c.data)],
+                    'pump1': [str(self.pump1_25c.data)],
+                    'pump2': [str(self.pump2_25c.data)],
+                    'dewpoint': [str(self.dewpoint_25c.data)] }
+        df_25c =  pd.DataFrame(data=data25c)
+        
+        fname25c = '/var/www/html/daf-monitor/compressor_data_25c.csv' 
+        if os.path.isfile(fname25c):
+            df_25c.to_csv(fname25c, mode='a', index=False, header=False)
+        else:
+            df_25c.to_csv(fname25c, mode='a', index=False, header=True)
+            
+        # Building 26
+        data26 = { 'date': [self.date_26.data.strftime('%Y-%m-%d %H:%M:%S')],
+                   'compressor': [str(self.compressor_26.data)],
+                   'dewpoint': [str(self.dewpoint_26.data)] }
+        df_26 =  pd.DataFrame(data=data26)
+        
+        fname26 = '/var/www/html/daf-monitor/compressor_data_26.csv' 
+        if os.path.isfile(fname26):
+            df_26.to_csv(fname26, mode='a', index=False, header=False)
+        else:
+            df_26.to_csv(fname26, mode='a', index=False, header=True)
